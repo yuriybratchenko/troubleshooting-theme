4191427d04314bcaac797f13819555a6223dbc9a
Yurii Bratchenko <bratchenkoyuriy@gmail.com>
2020-10-10 00:43:42 +0300

    UPD: footer

--- footer.php
+++ footer.php
@@ -12,18 +12,13 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'troubleshooting' ) ); ?>">
+		<div class="container">
+			<div class="site-info row">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'troubleshooting' ), 'WordPress' );
+					troubleshooting_footer_copyright();
+					troubleshooting_footer_social();
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'troubleshooting' ), 'troubleshooting', '<a href="http://underscores.me/">Yurii Bratchenko</a>' );
-				?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
